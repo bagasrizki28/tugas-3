7c0ec28e4480bd3cf9683cb71f31c8371ff8299c
Anggi Permana <anggipermana490@gmail.com>
2023-06-05 20:45:24 +0700

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const products = [
   {
     id: 1,
     name: "Mie Ayam Polos",
+    deskripsi: "Mie dengan potongan ayam yang enak dan nikmat! hanya Rp.20.000",
     price: "20.000",
     image:
       "https://awsimages.detik.net.id/community/media/visual/2022/10/12/resep-mie-ayam-kecap_43.jpeg?w=1200",
@@ -11,6 +12,8 @@ const products = [
   {
     id: 2,
     name: "Mie Ayam Bakso",
+    deskripsi:
+      "Perpaduan mie ayam dan bakso yang mengenyangkan, hanya Rp.25.000!",
     price: "25.000",
     image:
       "https://selerasa.com/wp-content/uploads/2015/05/images_mie_Mie_ayam_15-mie-ayam-bakso.jpg",
@@ -18,6 +21,8 @@ const products = [
   {
     id: 3,
     name: "Bakso Iga Seuhah",
+    deskripsi:
+      "Bakso dengan Daging tulang iga besar dan pedas! hanya Rp.30.000",
     price: "30.000",
     image:
       "https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2023/03/08/4251615582.jpg",
@@ -39,7 +44,7 @@ function App() {
           <div
             key={product.id}
             className="card"
-            onClick={() => handleProductClick(product.name)}
+            onClick={() => handleProductClick(product.deskripsi)}
           >
             <img src={product.image} alt={product.name} />
             <h2>{product.name}</h2>
